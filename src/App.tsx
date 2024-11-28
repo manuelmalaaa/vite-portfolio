b232a53b1a23508526439a70b0a2cc1d3fb337f9
Malavasi Manuel <malavasi.manuel02@gmail.com>
2024-11-28 22:23:16 +0100

    circle-container fixed + useintersect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import "./styles/colors.css";
+import { useIntersection } from "@mantine/hooks";
 
 interface Circles {
   id: number;
@@ -15,6 +16,7 @@ interface Circles {
 
 function App() {
   const [circles, setCircles] = useState<Circles[]>([]);
+  const { ref, entry } = useIntersection();
 
   useEffect(() => {
     const generateCircle = (id: number): Circles => {
@@ -54,7 +56,7 @@ function App() {
         gradient: `linear-gradient(${randomDirection()}, var(--purple700), var(--purple600))`,
         directionX,
         directionY,
-        speed: Math.random() * 1 + 0.5, // Velocità casuale
+        speed: Math.random() * 1 + 0.5,
       };
     };
 
@@ -90,7 +92,6 @@ function App() {
       });
     }, 16);
 
-    // Pulizia all'unmount
     return () => clearInterval(interval);
   }, []);
 
@@ -110,24 +111,50 @@ function App() {
 
   return (
     <div className="App">
-      <div className="circle-container">
-        {circles.map((circle) => (
-          <div
-            key={circle.id}
-            className="circle"
-            style={{
-              width: `${circle.size}px`,
-              height: `${circle.size}px`,
-              top: `${circle.y}px`,
-              left: `${circle.x}px`,
-              background: circle.gradient,
-            }}
-          />
-        ))}
-      </div>
-      <div className="container">
-        <h1>MALAVASI MANUEL</h1>
+      {entry?.isIntersecting && (
+        <div className="circle-container">
+          {circles.map((circle) => (
+            <div
+              key={circle.id}
+              className="circle"
+              style={{
+                width: `${circle.size}px`,
+                height: `${circle.size}px`,
+                top: `${circle.y}px`,
+                left: `${circle.x}px`,
+                background: circle.gradient,
+              }}
+            />
+          ))}
+        </div>
+      )}
+
+      <div className="start-view">
+        <h1 ref={ref}>MALAVASI MANUEL</h1>
         <h3>"Know your limits, but never stop trying to overcome them."</h3>
+      </div>
+
+      <div className="second-view">
+        <h1>Chi sono ?</h1>
+        <h3 style={{ width: "55%", marginTop: 60 }}>
+          Sono un Mobile developer con una grande passione per iOS e una solida
+          esperienza nell'uso di Swift, SwiftUI e UIKit. Mi occupo anche dello
+          sviluppo per Android, utilizzando Kotlin, Jetpack Compose e XML. Il
+          cross-platform mi aﬀascina molto, e mi sto impegnando a fondo con
+          React Native, JavaScript e TypeScript per creare applicazioni
+          scalabili ed eﬃcienti su entrambe le piattaforme. Ho un interesse
+          crescente per il web development, il backend e il design delle
+          applicazioni. Sempre orientato al miglioramento continuo, sono alla
+          ricerca di opportunità che mi permettano di crescere sia
+          professionalmente che personalmente. Al di fuori del lavoro, mi
+          interessa anche il settore finanziario, dove cerco costantemente di
+          ampliare le mie competenze. Il bodybuilding è una passione che mi
+          aiuta a coltivare disciplina e resilienza, valori che applico anche
+          nel mio approccio professionale. Amo viaggiare, scoprire nuove culture
+          e conoscere persone. Inoltre, sono profondamente appassionato del
+          settore automotive, nel quale continuo a sviluppare le mie conoscenze
+          per rimanere sempre aggiornato sulle ultime innovazion
+        </h3>
       </div>
     </div>
   );
